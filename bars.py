6f681860ae2fe4d3ca30470f216b972a2f31a3cc
Kenyiro Anazagasty <kenyicode@gmail.com>
2020-10-31 02:26:00 -0400

    bars.py formats stock data in individual, properly formatted files

--- bars.py
+++ bars.py
@@ -10,8 +10,6 @@ symbols = [holding.split(',')[2].strip() for holding in holdings][1:]
 # Convert to comma-separated list
 symbols = ','.join(symbols)
 
-#print(symbols)
-
 # Use alpaca api to retrieve stock data with symbols list
 days_bar_url = "{}/day?symbols={}&limit=1000".format(config.BARS_URL, symbols)
 
@@ -19,15 +17,14 @@ r = requests.get(days_bar_url, headers=config.HEADERS)
 
 data = r.json()
 
-#print(json.dumps(r.json(), indent=4))
-
 for symbol in data:
+    # Creates new file for each stock ticker
     filename = 'data/ohlc/{}.txt'.format(symbol)
     f = open(filename, 'w+')
     f.write('Date,Open,High,Low,Close,Volume,OpenInterest\n')
-    #print(data[symbol])
 
     for bar in data[symbol]:
+        # Formatting stock data, along with converting unix timestamp to actual date
         t = datetime.fromtimestamp(bar['t'])
         day = t.strftime('%Y-%m-%d')
         
