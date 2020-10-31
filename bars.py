75709b0a00461a0409238e13bd61b021e6de5362
Kenyiro Anazagasty <kenyicode@gmail.com>
2020-10-31 02:24:15 -0400

    bars.py formats stock data in individual, properly formatted files

--- bars.py
+++ bars.py
@@ -1,4 +1,5 @@
 import config, requests, json
+from datetime import datetime 
 
 # Grabbing data from csv file
 holdings = open('data/qqq.csv').readlines()
@@ -9,11 +10,26 @@ symbols = [holding.split(',')[2].strip() for holding in holdings][1:]
 # Convert to comma-separated list
 symbols = ','.join(symbols)
 
-print(symbols)
+#print(symbols)
 
 # Use alpaca api to retrieve stock data with symbols list
 days_bar_url = "{}/day?symbols={}&limit=1000".format(config.BARS_URL, symbols)
 
 r = requests.get(days_bar_url, headers=config.HEADERS)
 
-print(json.dumps(r.json(), indent=4))
+data = r.json()
+
+#print(json.dumps(r.json(), indent=4))
+
+for symbol in data:
+    filename = 'data/ohlc/{}.txt'.format(symbol)
+    f = open(filename, 'w+')
+    f.write('Date,Open,High,Low,Close,Volume,OpenInterest\n')
+    #print(data[symbol])
+
+    for bar in data[symbol]:
+        t = datetime.fromtimestamp(bar['t'])
+        day = t.strftime('%Y-%m-%d')
+        
+        line = '{},{},{},{},{},{},{}\n'.format(day, bar['o'], bar['h'], bar['l'], bar['c'], bar['v'], 0.00)
+        f.write(line)
